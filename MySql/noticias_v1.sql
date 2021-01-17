a8e45bfd38b78dbddda3b7d4f88a580a5a1cb179
Kunlaboris <77015605+Kunlaboris@users.noreply.github.com>
2021-01-17 19:10:55 +0100

    modificaciones solicitadas en reunión del 14/02/21

---
+++ MySql/noticias_v1.sql
@@ -0,0 +1,149 @@
+CREATE DATABASE  IF NOT EXISTS `noticias_v1` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `noticias_v1`;
+-- MySQL dump 10.13  Distrib 8.0.22, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: noticias_v1
+-- ------------------------------------------------------
+-- Server version	8.0.22-0ubuntu0.20.04.3
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `categoria`
+--
+
+DROP TABLE IF EXISTS `categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categoria` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) COLLATE utf8mb4_spanish_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categoria`
+--
+
+LOCK TABLES `categoria` WRITE;
+/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `noticia`
+--
+
+DROP TABLE IF EXISTS `noticia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `noticia` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(200) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `foto` varchar(255) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `entradilla` varchar(255) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `texto` mediumtext COLLATE utf8mb4_spanish_ci NOT NULL,
+  `fecha_publicacion` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `id_usuario` int unsigned NOT NULL,
+  `id_categoria` int unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `noticia_id_usuario_fk` (`id_usuario`),
+  KEY `noticia_id_categoria_fk` (`id_categoria`),
+  CONSTRAINT `noticia_id_categoria_fk` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `noticia_id_usuario_fk` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `noticia`
+--
+
+LOCK TABLES `noticia` WRITE;
+/*!40000 ALTER TABLE `noticia` DISABLE KEYS */;
+/*!40000 ALTER TABLE `noticia` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(40) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `apellido_1` varchar(60) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `apellido_2` varchar(60) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `foto` varchar(150) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `biografia` varchar(255) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `email` varchar(60) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `nickname` varchar(30) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `contrasena` varchar(100) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `fecha_creacion` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `nickname` (`nickname`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario_noticia`
+--
+
+DROP TABLE IF EXISTS `usuario_noticia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario_noticia` (
+  `id_usuario` int unsigned NOT NULL,
+  `id_noticia` int unsigned NOT NULL,
+  `valoraciones_positivas` tinyint DEFAULT NULL,
+  `valoraciones_negativas` tinyint DEFAULT NULL,
+  `comentario` varchar(250) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `fecha_valoracion` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_usuario`,`id_noticia`),
+  KEY `usuario_noticia_id_noticia_fk` (`id_noticia`),
+  CONSTRAINT `usuario_noticia_id_noticia_fk` FOREIGN KEY (`id_noticia`) REFERENCES `noticia` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `usuario_noticia_id_usuario_fk` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario_noticia`
+--
+
+LOCK TABLES `usuario_noticia` WRITE;
+/*!40000 ALTER TABLE `usuario_noticia` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario_noticia` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-01-17 17:43:39
