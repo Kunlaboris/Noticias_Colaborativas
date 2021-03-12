2f2b517bf463aac16013c764c422cffed39e5e5e
hack36 <ppm0007@gmail.com>
2021-03-12 20:45:30 +0100

    Creacion de Noticia Desplegada carpeta NewsDisplayed

---
+++ front/src/components/NewsDisplayed.js
@@ -0,0 +1,61 @@
+import './NewsDisplayed.css';
+
+<div id="ppal">
+  <div id="publicacion">
+    <h1 class="encabezado">8 consejos para hacer fotos de humo de colores increíbles</h1>
+
+    <img src="gente2.jpg" class="imagen-1" />
+
+    <a class="temas">Categoría</a>
+
+    <div id="data">
+      <img src="./avatarkunlaboris.svg" class="imagen-2" />
+      <div id="author">
+        <h3>Autor:</h3>
+        <p class="name-last-name">Nombre Apellido1 Apellido2</p>
+        <p>
+          Biografía: escritor, poeta, ensayista y periodista español cuya obra se desarrolla fundamentalmente en lengua
+          gallega, aunque también las traduce a menudo él mismo al castellano, idioma en el que escribe artículos para
+          el diario El País.
+        </p>
+      </div>
+    </div>
+
+    <div id="data-publication">
+      <div id="publication">
+        <h4>FECHA DE PUBLICACIÓN:</h4>
+        <p class="day-and-time">
+          <span>25 de Marzo del 2021 </span>
+          <a> 15:48</a>
+        </p>
+        <div class="imagen-3">
+          <img src="avatarkunlaboris.svg" width="100px" />
+        </div>
+        <div class="imagen-4">
+          <img src="avatarkunlaboris.svg" width="100px" />
+        </div>
+      </div>
+
+      <div class="entradilla">
+        ¿Alguna vez has pensado en practicar y dominar las fotos de humo de colores como las que ves por Internet?
+        Debido a la creciente popularidad de la fotografía de humo, muchos fotógrafos de bodas han comenzado a
+        incorporar bombas de humo en sus sesiones de fotos, ya que se consiguen efectos guays.
+      </div>
+
+      <div id="texto-noticia">
+        <p>
+          El humo que se añade en nuestras fotos seguramente puede crear impacto en el espectador, ya sea en fotografía
+          de retrato como en paisajes. Ofrecen resultados divertidos, un fondo llamativo y un ambiente inmejorable.Por
+          ejemplo, el rastro del humo ayuda a enfatizar los movimientos durante el lanzamiento. Elegir la bomba de humo
+          correcta: Al seleccionar una bomba de humo de colores para su lanzamiento, lo primero de la que debes
+          asegurarte es de la seguridad.La bomba de humo que debe usarse para trabajar con fotografías no debe ser
+          demasiado caliente o de lo contrario puedes sufrir quemaduras. Existen bombas de humo que no estás preparadas
+          para sostenerlas en las manos.Para obtener el mejor efecto e tus fotos de humo de colores, asegúrate de saber
+          cómo se usa este material de forma creativa y segura.Además, debes tener en cuenta la irritación que el humo
+          puede causar en los ojos o la garganta. Las personas que formen parte de la sesión de fotos deben ser
+          conscientes.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>;
